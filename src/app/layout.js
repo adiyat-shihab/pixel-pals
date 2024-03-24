cdd881980a18f23c483b4c9fd552ccdd02fc0861
adiyat-shihab <mohammadshihab104@gmail.com>
2024-03-25 00:15:27 +0600

    project setup

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,11 @@
-import { Inter } from "next/font/google";
+import { Poppins as primaryFont } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import "@radix-ui/themes/styles.css";
+import { Theme } from "@radix-ui/themes";
+const inter = primaryFont({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800"],
+});
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +15,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Theme>{children}</Theme>
+      </body>
     </html>
   );
 }
