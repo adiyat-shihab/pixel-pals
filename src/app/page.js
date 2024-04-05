2e8413995dafe0120a82c363d5992003c64bdae8
adiyat-shihab <mohammadshihab104@gmail.com>
2024-04-06 02:27:20 +0600

    add authentication google

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,14 @@
+"use client";
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { signIn, useSession } from "next-auth/react";
 
 export default function Home() {
-  return <></>;
+  const { data } = useSession();
+  console.log(data);
+  return (
+    <>
+      <Button onClick={() => signIn("google")}>Login</Button>
+    </>
+  );
 }
