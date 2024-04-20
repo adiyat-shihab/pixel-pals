014accd51b2e82ef64fdd47a56fc960df1eda286
adiyat-shihab <mohammadshihab104@gmail.com>
2024-04-20 09:11:21 +0600

    adding custom loader

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -2,12 +2,12 @@
 import { rawg } from "@/components/utils/rawg";
 import { Card, CardHeader } from "@/components/ui/card";
 import MainImage from "@/components/ui/MainImage";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useInfiniteQuery } from "@tanstack/react-query";
+import { Skeleton } from "@radix-ui/themes";
+import Image from "next/image";
 
 const Page = () => {
-  const [hasMore, setHasMore] = useState(true);
-  const [gameLength, setGameLength] = useState(0);
   const {
     data: games,
     fetchNextPage,
@@ -20,17 +20,16 @@ const Page = () => {
     queryFn: rawg,
     initialPageParam: 1,
     getNextPageParam: (lastPage, pages) => {
-      if (!lastPage.next) return undefined; // Check if there's a next page URL
-
+      if (!lastPage.next) return undefined;
       const url = new URL(lastPage.next);
-      // Assuming 'page' is the query parameter for pagination
       return url.searchParams.get("page");
     },
   });
+
   useEffect(() => {
     const interval = setInterval(() => {
       fetchNextPage();
-    }, 1500); // 10000 milliseconds = 10 seconds
+    }, 2500); // 10000 milliseconds = 10 seconds
 
     return () => clearInterval(interval);
   }, []);
@@ -51,23 +50,52 @@ const Page = () => {
     //   hasMore={hasMore}
     // >
     <div className={"text-white grid grid-cols-4 px-20 gap-y-9 "}>
-      {games?.pages?.map((page) =>
-        page.results.map((game) => {
-          return (
-            <Card
-              key={game.id}
-              className={"w-fit border-neutral-800 bg-neutral-800 mx-auto  "}
+      {!games ? (
+        <div className={" w-full h-screen bg-[#121212]"}>
+          <div
+            className={
+              " absolute  border-gray-800   transform left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
+            }
+            role="status"
+          >
+            <svg
+              aria-hidden="true"
+              class="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-gray-600 dark:fill-gray-300"
+              viewBox="0 0 100 101"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
             >
-              <CardHeader className={"p-0 border-neutral-800  "}>
-                <MainImage
-                  game_image={game?.background_image}
-                  game_name={game.name}
-                  custom_style={"object-cover rounded-lg h-48 w-[22rem]"}
-                />
-              </CardHeader>
-            </Card>
-          );
-        }),
+              <path
+                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                fill="currentColor"
+              />
+              <path
+                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                fill="currentFill"
+              />
+            </svg>
+            <span class="sr-only">Loading...</span>
+          </div>
+        </div>
+      ) : (
+        games?.pages?.map((page) =>
+          page.results.map((game) => {
+            return (
+              <Card
+                key={game.id}
+                className={"w-fit border-neutral-800 bg-neutral-800 mx-auto  "}
+              >
+                <CardHeader className={"p-0 border-neutral-800  "}>
+                  <MainImage
+                    game_image={game?.background_image}
+                    game_name={game.name}
+                    custom_style={"object-cover rounded-lg h-48 w-[22rem]"}
+                  />
+                </CardHeader>
+              </Card>
+            );
+          }),
+        )
       )}
     </div>
     // </InfiniteScroll>
